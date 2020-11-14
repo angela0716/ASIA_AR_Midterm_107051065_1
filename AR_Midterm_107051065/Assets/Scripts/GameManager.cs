b19a2bd34193ce4d08253e539548be2c6dd1ab47
angela0716 <72055214+angela0716@users.noreply.github.com>
2020-11-14 20:39:18 +0800

    期中作業

--- AR_Midterm_107051065/Assets/Scripts/GameManager.cs
+++ AR_Midterm_107051065/Assets/Scripts/GameManager.cs
@@ -4,5 +4,50 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    Animator
+    [Header("男人")]
+    public Transform man;
+    [Header("女人")]
+    public Transform women;
+    [Header("虛擬搖桿")]
+    public bl_Joystick joystick;
+    [Header("旋轉速度")]
+    [Range(0.1f, 15f)]
+    public float turn = 0.5f;
+    [Header("縮放")]
+    [Range(0f, 5f)]
+    public float size = 0.1f;
+    [Header("男人動畫元件")]
+    public Animator animan;
+    [Header("女人動畫元件")]
+    public Animator aniwomen;
+
+    private void Start()
+    {
+        print("開始事件");
+    }
+    private void Update()
+    {
+        print("更新事件");
+        print(joystick.Vertical);
+        man.Rotate(0, joystick.Horizontal * turn, 0);
+        women.Rotate(0, joystick.Horizontal * turn, 0);
+
+        man.localScale += new Vector3(1, 1, 1) * joystick.Vertical * size;
+        man.localScale = new Vector3(1, 1, 1) * Mathf.Clamp(man.localScale.x, 0.5f, 4f);
+        women.localScale += new Vector3(1, 1, 1) * joystick.Vertical * size;
+        women.localScale = new Vector3(1, 1, 1) * Mathf.Clamp(women.localScale.x, 0.5f, 4f);
+    }
+    public void run()
+    {
+        print("跑");
+        animan.SetTrigger("跑");
+        aniwomen.SetTrigger("跑");
+    }
+    public void PlayAnimation(string aniName)
+    {
+        print(aniName);
+        animan.SetTrigger(aniName);
+        aniwomen.SetTrigger(aniName);
 }
+}
+
